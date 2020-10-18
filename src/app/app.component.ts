aefc85fad8b18297d791a43a819c6bbc78c74cd9
Razvan Dragomir <drazvan91@gmail.com>
2020-10-18 18:36:29 +0300

    Removed unused components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
   ) {
     this.initializeApp();
   }
@@ -22,33 +22,6 @@ export class AppComponent {
     this.platform.ready().then(async () => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-
-      //   const result = await Diagnostic.getPermissionAuthorizationStatus(
-      //     Diagnostic.permission.READ_SMS
-      //   );
-      //   console.log(result);
-      //   Diagnostic.requestRuntimePermission(Diagnostic.permission.READ_SMS).then(
-      //     (data) => {
-      //       console.log(`getCameraAuthorizationStatus`);
-      //       console.log(data);
-      //     }
-      //   );
-
-      //   this.androidPermissions
-      //     .checkPermission(this.androidPermissions.PERMISSION.READ_SMS)
-      //     .then(
-      //       (result) => console.log('Has permission?', result.hasPermission),
-      //       (err) => {
-      //         this.androidPermissions.requestPermission(
-      //           this.androidPermissions.PERMISSION.READ_SMS
-      //         );
-      //       }
-      //     );
-
-      //   this.androidPermissions.requestPermissions([
-      //     this.androidPermissions.PERMISSION.CAMERA,
-      //     this.androidPermissions.PERMISSION.GET_ACCOUNTS,
-      //   ]);
     });
   }
 }
